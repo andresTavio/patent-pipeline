4a1b62606451c215997c9046386cb712d9b82421
Andres Tavio <andres.tavio@gmail.com>
2021-01-15 11:07:02 -0500

    cleaning up test spark dir

--- spark/entrypoint.py
+++ spark/entrypoint.py
@@ -9,8 +9,13 @@ spark = SparkSession \
         .getOrCreate()
 
 df = spark.read.json('app/files/raw_patents.json')
-df = df.select(explode(df.patents))
-df.explain()
-df.printSchema()
-df.show()
-df.write.mode('overwrite').parquet('app/files/raw_patents')
+df_exploded = df.select(explode(df.patents))
+df_exploded = df_exploded.withColumn('date', df_exploded.col.patent_date) \
+    .withColumn('year', df_exploded.col.patent_year) \
+    .withColumn('title', df_exploded.col.patent_title) \
+    .withColumn('type', df_exploded.col.patent_type) \
+    .withColumn('processing_time', df_exploded.col.patent_average_processing_time)
+df_exploded.explain()
+df_exploded.printSchema()
+df_exploded.show()
+df_exploded.write.mode("overwrite").parquet('app/files/raw_patents')
